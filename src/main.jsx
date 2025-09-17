c89391c32a55da972d91768011235e3b7365b6de
TheUltimateVxnom <lukasdiezinger3@gmail.com>
2025-09-17 13:37:32 +0200

    main.jsx got updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css"; // falls du Tailwind verwendest
+import App from "./App.jsx";  // ./App.jsx, NICHT /src/App.jsx
+import "./index.css";          // falls du Tailwind nutzt
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
